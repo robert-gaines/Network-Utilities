562b17db26d3855156a69d896a6ebff88baa9335
robert-gaines <robert.gaines@hushmail.com>
2022-03-15 21:49:23 -0700

    Routine - minor adjustments to PS GUI

--- Enumeration/GraphicalPingSweep/PingSweep.py
+++ Enumeration/GraphicalPingSweep/PingSweep.py
@@ -23,20 +23,17 @@ class PingWorker(QObject):
     target_status_down = pyqtSignal()
     target_status_up   = pyqtSignal()
 
-    def __init__(self,target,tx_bytes):
+    def __init__(self,target):
         super().__init__()
-        self.target           = target
-        self.bytes_per_packet = tx_bytes
+        self.target = target
 
     def SendPing(self):
         startupinfo = subprocess.STARTUPINFO()
         startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        tx_bytes = str(self.bytes_per_packet)
-        self.increment_tx_data.emit()
         if(os.name == 'nt'):
-            output = subprocess.Popen(['ping','-n','1','-w','150','-l',tx_bytes,str(self.target)],startupinfo=startupinfo,stdout=subprocess.PIPE).communicate()[0]
+            output = subprocess.Popen(['ping','-n','1','-w','150',str(self.target)],startupinfo=startupinfo,stdout=subprocess.PIPE).communicate()[0]
         else:
-            output = subprocess.Popen(['ping','-c','1','-w','150','-s',tx_bytes,str(self.target)],startupinfo=startupinfo,stdout=subprocess.PIPE).communicate()[0]
+            output = subprocess.Popen(['ping','-c','1','-w','150',str(self.target)],startupinfo=startupinfo,stdout=subprocess.PIPE).communicate()[0]
         if('Reply' in output.decode('utf-8')):
             self.target_status_up.emit()
         else:
@@ -47,6 +44,7 @@ class Window(QWidget):
     def __init__(self,parent=None):
         #
         self.hosts_to_scan = []
+        self.live_hosts    = []
         #
         ipv4_subnets = [
         "255.255.255.255",
@@ -91,7 +89,7 @@ class Window(QWidget):
         QLabel.__init__(self)
         #
         self.setWindowTitle('Graphical Ping Sweep')
-        self.setGeometry(850,400,500,500)
+        self.setGeometry(600,200,500,800)
         self.setStyleSheet("background-color: gray; border: 2px black solid")
         #
         self.total_bytes_sent     = 0
@@ -100,8 +98,20 @@ class Window(QWidget):
         self.target_network_label           = QLabel("Network Address")
         self.target_network_label.setStyleSheet("height: 25px; width: 50px; color: White; font-style: bold; font-size: 16px; font-family: Arial")                                    
         self.target_network_address         = QLineEdit()                                          
-        self.target_network_address.setPlaceholderText("(Target Address)")
+        self.target_network_address.setPlaceholderText("(Network Address)")
         self.target_network_address.setStyleSheet("height: 25px; width: 50px; background-color: black; color: White; border: 2px solid black; border-radius: 10px; font-style: bold; font-size: 16px; font-family: Arial")
+        self.elapsed_time_label             = QLabel("Elapsed Time")
+        self.elapsed_time_label.setStyleSheet("height: 25px; width: 50px; color: White; font-style: bold; font-size: 16px; font-family: Arial")
+        self.elapsed_time         = QLineEdit() 
+        self.elapsed_time.setFixedWidth(75)                                         
+        self.elapsed_time.setPlaceholderText("00:00:00")
+        self.elapsed_time.setStyleSheet("height: 25px; width: 25px; background-color: black; color: White; border: 2px solid black; border-radius: 10px; font-style: bold; font-size: 16px; font-family: Arial") 
+        self.live_hosts_label     = QLabel("Live Hosts")
+        self.live_hosts_label.setStyleSheet("margin-left: 5px; height: 25px; width: 50px; color: White; font-style: bold; font-size: 16px; font-family: Arial") 
+        self.live_hosts_field         = QLineEdit()
+        self.live_hosts_field.setFixedWidth(75)                                          
+        self.live_hosts_field.setPlaceholderText("0")
+        self.live_hosts_field.setStyleSheet("; height: 25px; width: 25px; background-color: black; color: White; border: 2px solid black; border-radius: 10px; font-style: bold; font-size: 16px; font-family: Arial")
         #
         self.stop_button = QPushButton("Stop", self)
         self.stop_button.setGeometry(100,100,600,400)
@@ -111,9 +121,9 @@ class Window(QWidget):
         self.ping_button.setGeometry(100,100,600,400)
         self.ping_button.setStyleSheet("margin-top: 35px; height: 50px; width: 50px; background-color: green; color: black; border: 2px solid black; border-radius: 10px; font-style: bold; font-size: 24px; font-family: Arial")
         #
-        self.ping_count_label        = QLabel("Subnet Mask")
-        self.ping_count_label.setStyleSheet("height: 25px; width: 50px; color: White; font-style: bold; font-size: 16px; font-family: Arial") 
-        self.subnet_combo_box        = QComboBox()
+        self.subnet_label = QLabel("Subnet Mask")
+        self.subnet_label.setStyleSheet("height: 25px; width: 50px; color: White; font-style: bold; font-size: 16px; font-family: Arial") 
+        self.subnet_combo_box = QComboBox()
         self.subnet_combo_box.setStyleSheet("height: 25px; width: 25px; background-color: Black; color: White; border: 2px solid black; border-radius: 10px; font-style: bold; font-size: 16px; font-family: Arial")
         #
         for mask in ipv4_subnets:
@@ -131,6 +141,8 @@ class Window(QWidget):
         #
         main_layout                = QFormLayout()
         self.vertical_button_box   = QVBoxLayout()
+        self.horizontal_data_box   = QHBoxLayout()
+        self.vertical_table_box    = QVBoxLayout()
         self.horizontal_button_box = QHBoxLayout()
         #
         self.vertical_button_box.setSpacing(15)
@@ -138,18 +150,26 @@ class Window(QWidget):
         #
         self.vertical_button_box.addWidget(self.target_network_label)
         self.vertical_button_box.addWidget(self.target_network_address)
-        self.vertical_button_box.addWidget(self.ping_count_label)
+        self.vertical_button_box.addWidget(self.subnet_label)
         self.vertical_button_box.addWidget(self.subnet_combo_box)
-        self.vertical_button_box.addWidget(self.tableWidget)
+        #
+        self.horizontal_data_box.addWidget(self.elapsed_time_label)
+        self.horizontal_data_box.addWidget(self.elapsed_time)
+        self.horizontal_data_box.addWidget(self.live_hosts_label)
+        self.horizontal_data_box.addWidget(self.live_hosts_field)
+        #
+        self.vertical_table_box.addWidget(self.tableWidget)
         #
         self.horizontal_button_box.addWidget(self.stop_button)
         self.horizontal_button_box.addWidget(self.ping_button)
         #
         main_layout.setVerticalSpacing(10)
         main_layout.addRow(self.vertical_button_box)
+        main_layout.addRow(self.horizontal_data_box)
+        main_layout.addRow(self.vertical_table_box)
         main_layout.addRow(self.horizontal_button_box)
         #
-        self.ping_button.clicked.connect(self.InitializeHosts)
+        self.ping_button.clicked.connect(self.ExecuteSweep)
         self.stop_button.clicked.connect(self.Terminate)
         #
         self.setLayout(main_layout)
@@ -171,34 +191,50 @@ class Window(QWidget):
             self.target_network += "/"
             self.target_network += subnet_mask
             self.hosts_to_scan = ipaddress.IPv4Network(self.target_network).hosts()
-            for h in self.hosts_to_scan:
-                entry = [h,'UP']
-                self.AddTableRow(self.tableWidget,entry)
-                logging.info('%s' % h)
         except Exception as e:
             logging.info('%s' % e)
             sys.exit(1)
 
-
     def SetStatusAlive(self):
-        return
+        current_row = self.tableWidget.rowCount()
+        self.tableWidget.setRowCount(current_row+1)
+        col_index   = 0
+        host_addr   = str(self.current_host)
+        host_stat   = 'Alive'
+        cell_value  = QTableWidgetItem(host_addr)
+        self.tableWidget.setItem(current_row,col_index,cell_value) 
+        col_index   = 1
+        cell_value  = QTableWidgetItem(host_stat)
+        self.tableWidget.setItem(current_row,col_index,cell_value)
+        self.tableWidget.update() 
 
     def SetStatusDown(self):
-        return
+        current_row = self.tableWidget.rowCount() 
+        self.tableWidget.setRowCount(current_row+1)
+        col_index   = 0
+        host_addr   = str(self.current_host)
+        host_stat   = 'Down'
+        cell_value  = QTableWidgetItem(host_addr)
+        self.tableWidget.setItem(current_row,col_index,cell_value) ; logging.info("%s:%s:%s" % (current_row,col_index,cell_value.text()))
+        col_index   = 1
+        cell_value  = QTableWidgetItem(host_stat)
+        self.tableWidget.setItem(current_row,col_index,cell_value) ; logging.info("%s:%s:%s" % (current_row,col_index,cell_value.text()))
+        self.tableWidget.update() 
 
-    def UpdateTotalDataDisplay(self):
-        return
-
-    def ExecutePings(self):
-        #threadCount = QThreadPool.globalInstance().maxThreadCount()
-        #threadPool  = QThreadPool.globalInstance()
-        #for i in range(int(self.ping_count_combo_box.currentText())):
-            # self.total_bytes_sent_str = str(temp_converted_bytes)
-            # self.worker = PingWorker(self.target_address.text(),self.tx_byte_count.text())
-            # self.worker.target_status_up.connect(self.SetStatusAlive)
-            # self.worker.target_status_down.connect(self.SetStatusDown)
-            # self.worker.increment_tx_data.connect(self.UpdateTotalDataDisplay)
-            # threadPool.start(self.worker.SendPing)
+    def ExecuteSweep(self):
+        self.current_host = ''
+        self.InitializeHosts()
+        self.start_time       = time.perf_counter()
+        threadCount = QThreadPool.globalInstance().maxThreadCount()
+        threadPool  = QThreadPool.globalInstance()
+        for addr in self.hosts_to_scan:
+            self.current_host = addr
+            logging.info("%s" % self.current_host)
+            self.worker = PingWorker(self.current_host)
+            self.worker.target_status_up.connect(self.SetStatusAlive)
+            self.worker.target_status_down.connect(self.SetStatusDown)
+            threadPool.start(self.worker.SendPing)
+            current_time = time.perf_counter()
         return
 
     def Terminate(self):
